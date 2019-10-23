a00c34e1d7df943b28c2cb9eb837293bf11daedc
Oleg Balabukh <olegukoff@gmail.com>
2019-10-24 01:05:23 +0300

    Updates

---
+++ precache-manifest.e1270e93fa23f466c3f3fa6474dc7312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513b7640a6422ed0d5a39b55ee64c41e",
+    "url": "/body_mass_index_react/index.html"
+  },
+  {
+    "revision": "0898c95d46453360377c",
+    "url": "/body_mass_index_react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1bf98837b4cf6e4f8a23",
+    "url": "/body_mass_index_react/static/css/main.59440f89.chunk.css"
+  },
+  {
+    "revision": "0898c95d46453360377c",
+    "url": "/body_mass_index_react/static/js/2.2f3ea92d.chunk.js"
+  },
+  {
+    "revision": "1bf98837b4cf6e4f8a23",
+    "url": "/body_mass_index_react/static/js/main.af9aa32a.chunk.js"
+  },
+  {
+    "revision": "7c28f218d5159ef11f7f",
+    "url": "/body_mass_index_react/static/js/runtime~main.86623bdb.js"
+  }
+]);
